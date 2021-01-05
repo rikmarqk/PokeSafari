87e73d3ca075b9ae8701fa1a6501a71dcb8bb800
akrama23 <arundelfootball23@gmail.com>
2021-01-05 18:12:15 -0500

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,20 @@
 class SessionsController < ApplicationController
     def new 
-
+        @user = User.new
     end 
 
     def create 
-
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:id] = @user.id 
+            redirect_to user_path(@user)
+        else  
+            flash[:errors] = ["Email Or Password Is Incorrect"]
+            redirect_to login_path
+        end 
     end 
     def destroy 
-        
+        session.clear 
+        redirect_to login_path
     end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    # has_secure_password
 
-
+    before_action :not_logged_in, only: [:show]
+    
     def index
         @users = User.all
     end
