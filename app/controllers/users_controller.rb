97ee66b457d115729e7da30c2c27402c87ae4f16
rikmarqk <rikmarqk@gmail.com>
2021-01-05 15:59:12 -0600

    commit 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,26 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+    def new
+        @user = User.new  
+    end 
+    def create
+        @user = User.new (user_params)
+        if  @user.valid?
+            @user.save
+            sessions[:id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end 
+    end 
     def show
         @user = User.find(params[:id])
     end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :email, :img_url, :password, :password_conformation)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root ‘home#index’
-  resources :users, :pokemons, :zones
+  resources :users, only: [:index, :new, :show, :create]
+  resources :pokemons, :zones
     
+  get '/login', to: 'sessions#new', as:'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'session#destroy', as:''
 end
